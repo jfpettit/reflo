9562977b4567e816a0b239b99670cef4b7b7fab1
Jacob Pettit <jacobpettit@jacobs-mbp.attlocal.net>
2019-09-22 23:49:30 -0700

    update README

---
+++ __init__.py
@@ -0,0 +1,6 @@
+name = "rlpacktf"
+
+__all__ = ['a2c', 'ppo']
+
+from . import a2c
+from . import ppo
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
 	author_email='jfpettit@gmail.com',
     short_description='TensorFlow implementations of some reinforcement learning algorithms. Intended to be user friendly.',
     long_description=long_description,
-	url='https://github.com/jfpettit/rlpac-tfk',
+	url='https://github.com/jfpettit/rlpack-tf',
 	install_requires=['numpy', 'tensorflow', 'gym', 'scipy', 'dm-sonnet',
 		'tensorflow-probability', 'pandas', 'matplotlib', 'roboschool']
 	,
